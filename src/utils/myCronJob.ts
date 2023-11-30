844f7a0f1c95b3c08a3618b2e84e3c7a80718020
lale <alejandracolombini1287@gmail.com>
2023-11-30 16:13:33 -0300

    cronjob site

--- src/utils/myCronJob.ts
+++ src/utils/myCronJob.ts
@@ -1,16 +1,16 @@
 import {CronJob, cronJob} from '@loopback/cron';
 import {repository} from '@loopback/repository';
-import {File} from '../models';
-import {FileRepository} from '../repositories/file.repository';
+import {Site} from '../models';
+import {SiteRepository} from '../repositories';
 
 @cronJob()
 export class MyCronJob extends CronJob {
-  constructor(@repository(FileRepository) public fileRepository: FileRepository,) {
+  constructor(@repository(SiteRepository) public siteRepository: SiteRepository,) {
     super({
       name: 'job-B', onTick: async () => {
-        let files: File[] = await fileRepository.find();
+        let sites: Site[] = await siteRepository.find();
         console.log(new Date());
-        console.log(files);
+        console.log(sites);
       },
       cronTime: '*/10 * * * * *',
       start: true,
